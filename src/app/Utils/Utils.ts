2c9662b72c02f4c2a9d1c7c0522cb63a131525bf
Bruno Vieira <vieira.es@gmail.com>
2022-04-01 03:58:15 -0300

    added: simple sistem example to make tests

---
+++ src/app/Utils/Utils.ts
@@ -0,0 +1,70 @@
+import { UrlWithParsedQuery, parse } from "url";
+import { IncomingMessage } from "http";
+
+
+export class Utils {
+
+
+
+    // Dummy object test example
+    public static getRequestBasePath(req: IncomingMessage): string {
+        const url = req.url;
+        if (url) {
+            const parsedUrl = this.parseUrl(url);
+            if (parsedUrl.pathname) {
+                return parsedUrl.pathname.split('/')[1];
+            } else {
+                return ''
+            }
+        } else {
+            return '';
+        }
+    }
+
+    public static async getRequestBody(request: IncomingMessage): Promise<any> {
+        return new Promise((resolve, reject) => {
+            let body = '';
+            request.on('data', (data: string) => {
+                body += data;
+            });
+            request.on('end', () => {
+                try {
+                    resolve(JSON.parse(body));
+                } catch (jsonError) {
+                    reject(jsonError)
+                }
+            });
+            request.on('error', (error: any) => {
+                reject(error)
+            });
+        });
+    }
+
+    //// aqui abaixo é o codigo da primeira versao do aprendizado
+
+    public static parseUrl(url: string): UrlWithParsedQuery {
+        if (!url) {
+            throw new Error('Invalid URL');
+        }
+        return parse(url, true);
+
+    }
+
+
+    public static toUpperCase(argument: string): string {
+        return argument.toUpperCase();
+    }
+
+
+    /*istanbul ignore next*/
+    public static compareIfTrue(valor: boolean): void {
+
+
+        expect(valor).toBe(true);
+
+
+    }
+
+
+
+}
